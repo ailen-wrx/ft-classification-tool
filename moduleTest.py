7b0e592cf8f81a9ff2d6e0391bfad08473cee042
Ruixin Wang <wangrx1999@gmail.com>
2021-07-20 00:12:17 +0800

    improve module structure

--- moduleTest.py
+++ moduleTest.py
@@ -1,4 +1,6 @@
 class moduleTest(object):
-    def __init__(self, moduleDir):
-        self.moduleDir = moduleDir
+    def __init__(self, moduleName):
+        self.moduleName = moduleName
+        self.directory = ''
+        self.repository = ''
         self.testSet = []
