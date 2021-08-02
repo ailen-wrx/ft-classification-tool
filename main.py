bc432afbc237cb8284694f0031af05f61be6f5b5
Ruixin Wang <wangrx1999@gmail.com>
2021-08-02 15:57:13 +0800

    patch

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from parseMethods import *
 from statMethods import *
-from patch import  *
+from patch import *
 
 dataset_path = "D:\\iDoFT\\dataset"
 
-stat_init()
-list_flaky = init_flaky()
-parse_filelist(dataset_path, list_flaky)
+# stat_init()
+# list_flaky = init_flaky()
+# parse_filelist(dataset_path, list_flaky)
+#
+# patch()
+# match_flaky()
 
-patch()
-match_flaky()
